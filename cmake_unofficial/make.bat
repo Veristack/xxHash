68c80aab39ff9170271ab502e41d2144edae43be
Giampaolo Rodola <g.rodola@gmail.com>
2018-02-28 19:59:12 +0100

    fix cmake for mingw

--- cmake_unofficial/make.bat
+++
@@ -1,8 +0,0 @@
-set PATH=C:\mingw\bin;%PATH%
-set CC=C:\MinGW\bin\gcc.exe
-set CXX=C:\MinGW\bin\gcc.exe
-set MINGWMAKE=C:\MinGW\bin\mingw32-make.exe
-
-del /F CMakeCache.txt
-cmake -DBUILD_XXHSUM=OFF -G "MinGW Makefiles" .
-%MINGWMAKE%
